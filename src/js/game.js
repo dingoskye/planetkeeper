b1f69b5b3aa625001a80a392b37e10f4ff0d0d17
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-06-11 10:24:13 +0200

    image check

--- src/js/game.js
+++ src/js/game.js
@@ -16,12 +16,40 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+        const world = new Actor()
+        world.graphics.use(Resources.World.toSprite())
+        world.pos = new Vector(500, 300)
+        world.vel = new Vector(-10,0)
+        world.events.on("exitviewport", (e) => this.fishLeft(e))
+        this.add(world)
+
+        const world2 = new Actor()
+        world2.graphics.use(Resources.WorldStage2.toSprite())
+        world2.pos = new Vector(400, 300)
+        world2.vel = new Vector(-10,0)
+        world2.events.on("exitviewport", (e) => this.fishLeft(e))
+        this.add(world2)
+
+        const world3 = new Actor()
+        world3.graphics.use(Resources.WorldStage3.toSprite())
+        world3.pos = new Vector(300, 300)
+        world3.vel = new Vector(-10,0)
+        world3.events.on("exitviewport", (e) => this.fishLeft(e))
+        this.add(world3)
+
+        const world4 = new Actor()
+        world4.graphics.use(Resources.WorldStage4.toSprite())
+        world4.pos = new Vector(200, 300)
+        world4.vel = new Vector(-10,0)
+        world4.events.on("exitviewport", (e) => this.fishLeft(e))
+        this.add(world4)
+
+        const world5 = new Actor()
+        world5.graphics.use(Resources.WorldDead.toSprite())
+        world5.pos = new Vector(600, 300)
+        world5.vel = new Vector(-10,0)
+        world5.events.on("exitviewport", (e) => this.fishLeft(e))
+        this.add(world5)
     }
 
     fishLeft(e) {
