92fea8ca6bb45696a34d83bf5e9f4b5b8c1ab54a
chrisieee <christa_pol1@hotmail.com>
2025-06-11 12:01:26 +0200

    Ui stuff

---
+++ src/js/background.js
@@ -0,0 +1,14 @@
+import { Actor, Vector } from "excalibur"
+import { Resources } from './resources.js'
+
+export class Bg extends Actor {
+    constructor() {
+        super({
+            x: 650, y: 190,
+            width: 1280, height: 720
+        })
+
+        this.graphics.use(Resources.Scenebg.toSprite())
+        this.scale = new Vector(0.6, 0.6)
+    }
+}
---
+++ src/js/ui/progressbar.js
@@ -0,0 +1,13 @@
+import { Actor, Vector } from "excalibur"
+import { Resources } from './resources.js'
+
+export class ProgressionBar extends Actor {
+
+    bar
+
+    constructor() {
+        super() //Just do it! 
+
+
+    }
+}
