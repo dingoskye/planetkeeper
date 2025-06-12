9af4b823e137cf3396231588584f16e321f83dfe
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-06-12 11:38:23 +0200

    small image fix and world begin

--- src/js/worlds/world_fase-1.js
+++ src/js/worlds/world_fase-1.js
@@ -2,10 +2,26 @@ import { Actor, Vector } from "excalibur"
 import { Resources } from '../resources.js'
 
 export class WorldFaseOne extends Actor {
+
+    developedCounter 
+    Resources
+    developed
+
     constructor() {
         super()
         this.graphics.use(Resources.World.toSprite())
         this.pos = new Vector(640, 350)
         this.scale = new Vector(5, 5)
+        this.developedCounter = 0
+    }
+
+    onPostUpdate(engine){
+        this.developedCounter ++
+        if (this.developedCounter >= 3600) {
+            this.developed --;
+            this.developedCounter = 0
+        }
+
+        
     }
 }
