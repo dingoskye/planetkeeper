f639269ae6b4206dd24bc4c2eabd8bb1a35e833b
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-06-12 14:15:46 +0200

    art update

--- src/js/worlds/world_fase-1.js
+++ src/js/worlds/world_fase-1.js
@@ -3,25 +3,25 @@ import { Resources } from '../resources.js'
 
 export class WorldFaseOne extends Actor {
 
-    developedCounter 
-    Resources
-    developed
+    progressionCounter 
+    resource
+    progression
 
     constructor() {
         super()
         this.graphics.use(Resources.World.toSprite())
         this.pos = new Vector(640, 350)
-        this.scale = new Vector(5, 5)
-        this.developedCounter = 0
+        this.scale = new Vector(1,1)
+        this.progressionCounter = 0
     }
 
     onPostUpdate(engine){
-        this.developedCounter ++
-        if (this.developedCounter >= 3600) {
-            this.developed --;
-            this.developedCounter = 0
+        this.progressionCounter ++
+        if (this.progressionCounter >= 3600) {
+            this.progression --;
+            this.progressionCounter = 0
         }
 
-        
+
     }
 }
