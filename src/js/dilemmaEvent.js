86af02a8564d844b08e29df6edf453954bc4d1a9
Skye Jacobs <skyejacobs@casema.nl>
2025-06-16 11:58:47 +0200

    Labels en tekst nu daadwerkelijk werkend, happy little violations

--- src/js/dilemmaEvent.js
+++ src/js/dilemmaEvent.js
@@ -15,11 +15,9 @@ export class DilemmaEvent extends Actor {
 
     onInitialize() {
         this.logJson();
+        this.dilemmaManager();
     }
 
-    onPostUpdate() {
-
-    }
 
     logJson() {
         for (let p of jsonData) {
@@ -28,19 +26,44 @@ export class DilemmaEvent extends Actor {
     }
 
     dilemmaManager() {
+        // Toon direct een random dilemma bij start
+        this.showRandomDilemma();
 
+        // Daarna elke minuut een nieuw dilemma
+        this.dilemmaInterval = setInterval(() => {
+            this.hideDilemma(); // Verberg het vorige dilemma
+            this.showRandomDilemma();
+        }, 2000); // 60.000 ms = 1 minuut
     }
 
-    showDilemma() {
-        // Haal het eerste dilemma-event uit de JSON
-        const event = jsonData[0];
+    showRandomDilemma() {
+        const randomIndex = Math.floor(Math.random() * jsonData.length);
+        const event = jsonData[randomIndex];
+        this.showDilemma(event);
+    }
+
+    showDilemma(event) {
+
+        this.hideDilemma(); // Verberg eventueel vorig dilemma
 
         // Achtergrond 
-        const bgWidth = 900;
-        const bgHeight = 250;
-        const bgX = 70 + bgWidth / 2;
-        const bgY = 890 + bgHeight / 2;
 
+        const canvasHeight = 720; // of this.scene.engine.drawHeight
+        const labelStartX = 20;
+        const labelStartY = canvasHeight - 170; // bijvoorbeeld
+        const labelSpacing = 40;
+        const labelCount = 4; // Dilemma + 3 keuzes
+
+        const bgPaddingX = 20;
+        const bgPaddingY = 20;
+
+        const bgWidth = 900; // of bijvoorbeeld 1240 als je bijna het hele canvas wilt
+        const bgHeight = (labelSpacing * (labelCount - 1)) + 40 + bgPaddingY * 2; // hoogte van alle labels + marge
+
+        const bgX = labelStartX + bgWidth / 2 - bgPaddingX;
+        const bgY = labelStartY + bgHeight / 2 - bgPaddingY;
+
+        // Maak de achtergrond precies passend
         this.labelScreen = new Actor({
             pos: new Vector(bgX, bgY),
             width: bgWidth,
@@ -48,20 +71,23 @@ export class DilemmaEvent extends Actor {
             z: 9000
         });
 
-        // Voeg sprite toe als graphics
-        const bgSprite = Resources.EventText.toSprite();
-        bgSprite.width = bgWidth;
-        bgSprite.height = bgHeight;
-        this.labelScreen.graphics.use(bgSprite);
+        const bgSprite = Resources.EventText?.isLoaded?.() ? Resources.EventText.toSprite() : null;
+        if (bgSprite) {
+            bgSprite.width = bgWidth;
+            bgSprite.height = bgHeight;
+            this.labelScreen.graphics.use(bgSprite);
+        } else {
+            this.labelScreen.color = Color.Blue;
+        }
 
         this.addChild(this.labelScreen);
 
         // Label voor het dilemma
         this.#LabelDilemma = new Label({
             text: event.title + "\n" + event.context,
-            pos: new Vector(100, 900),
+            pos: new Vector(20, 570),
             font: new Font({
-                size: 40,
+                size: 25,
                 family: 'Open Sans',
                 color: Color.White,
                 textAlign: TextAlign.Left,
@@ -74,9 +100,9 @@ export class DilemmaEvent extends Actor {
         // Labels voor de keuzes
         this.#labelA = new Label({
             text: "A: " + event.choices[0].option,
-            pos: new Vector(100, 980),
+            pos: new Vector(20, 630),
             font: new Font({
-                size: 30,
+                size: 20,
                 family: 'Open Sans',
                 color: Color.Yellow,
                 textAlign: TextAlign.Left,
@@ -88,9 +114,9 @@ export class DilemmaEvent extends Actor {
 
         this.#labelB = new Label({
             text: "B: " + event.choices[1].option,
-            pos: new Vector(100, 1030),
+            pos: new Vector(20, 650),
             font: new Font({
-                size: 30,
+                size: 20,
                 family: 'Open Sans',
                 color: Color.Yellow,
                 textAlign: TextAlign.Left,
@@ -102,9 +128,9 @@ export class DilemmaEvent extends Actor {
 
         this.#LabelC = new Label({
             text: "C: " + event.choices[2].option,
-            pos: new Vector(100, 1080),
+            pos: new Vector(20, 670),
             font: new Font({
-                size: 30,
+                size: 20,
                 family: 'Open Sans',
                 color: Color.Yellow,
                 textAlign: TextAlign.Left,
@@ -120,15 +146,35 @@ export class DilemmaEvent extends Actor {
             this.labelScreen.kill();
             this.labelScreen = null;
         }
-        if (this.endLabel) {
-            this.endLabel.kill();
-            this.endLabel = null;
+
+        // Zet alle labels in een array
+        const labels = [this.#LabelDilemma, this.#labelA, this.#labelB, this.#LabelC];
+
+        // Loop eroverheen en kill + null
+        for (let i = 0; i < labels.length; i++) {
+            if (labels[i]) {
+                labels[i].kill();
+                labels[i] = null;
+            }
         }
 
+        // Zet de private fields ook op null
+        this.#LabelDilemma = null;
+        this.#labelA = null;
+        this.#labelB = null;
+        this.#LabelC = null;
     }
 
 
     progressionHandler() {
+
+    }
+
+    reputationHandler() {
+
+    }
+
+    resourcesHandler() {
 
     }
 }
